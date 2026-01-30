bbe10f11fc709f265d040016cbac11a3b2d69861
Virendra Singh <virendrasinghnathawat9@gmailcom>
2026-01-30 12:09:23 +0530

    Fix auth route mounting and duplicate register route

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const {
   login,
 } = require("../controllers/authController");
 
-router.post("/register", register);
+// router.post("/register", register);
 router.post("/login", login);
 
 router.post("/register", async (req, res) => {
