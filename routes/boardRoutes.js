7c65197169de73ebb25ba32d5752d9e417744c50
Virendra Singh <virendrasinghnathawat9@gmailcom>
2026-01-30 11:21:03 +0530

    Remove env file and add gitignore

--- routes/boardRoutes.js
+++ routes/boardRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Board = require("../models/Board"); // ✅ THIS WAS MISSING
+const Board = require("../models/Board"); 
 
 const {
   createBoard,
