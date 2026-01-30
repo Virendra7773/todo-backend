dc784d727f9ea904b43d03610472cd993a0d2507
Virendra Singh <virendrasinghnathawat9@gmailcom>
2026-01-30 12:10:01 +0530

    Fix auth route mounting and duplicate register route

--- routes/auth.js
+++ routes/auth.js
@@ -6,33 +6,33 @@ const {
   login,
 } = require("../controllers/authController");
 
-// router.post("/register", register);
+router.post("/register", register);
 router.post("/login", login);
 
-router.post("/register", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+// router.post("/register", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ message: "All fields required" });
-    }
+//     if (!email || !password) {
+//       return res.status(400).json({ message: "All fields required" });
+//     }
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already registered" });
-    }
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return res.status(400).json({ message: "Email already registered" });
+//     }
 
-    const user = await User.create({ email, password });
+//     const user = await User.create({ email, password });
 
-    res.status(201).json({
-      message: "Registration successful",
-      userId: user._id,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+//     res.status(201).json({
+//       message: "Registration successful",
+//       userId: user._id,
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
 
 
 module.exports = router;
